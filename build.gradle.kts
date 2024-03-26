49eddd2313bdd651ceb83a7b67eee0c41c082e45
Belov, Mikhail <MikhailBelov@x5.ru>
2024-03-26 13:48:45 +0400

    init

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    id("org.openapi.generator") version "7.0.1"
+}
+
+group = "ru.belov"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.moshi:moshi:1.15.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.22")
+}
+
+openApiGenerate {
+    inputSpec.set("$rootDir/openapi/spec.yaml")
+    generatorName.set("kotlin")
+    groupId = "ru.belov"
+    id = "api"
+    version = "1.0.0"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
